b7e25cc460e2e801d0f6fa2c9261d3513ef7955c
Dmitriy Solovev <dmptts@gmail.com>
2020-12-10 13:07:51 +0300

    Вносит различные изменения

--- build/js/main.js
+++ build/js/main.js
@@ -168,6 +168,55 @@
 })();
 
 (function () {
+  var pageHeader = document.querySelector('.header');
+  var topContainer = document.querySelector('.header__top-container');
+  var menu = document.querySelector('.header__menu');
+  var mainNav = document.querySelector('.header__nav');
+  var search = document.querySelector('.search');
+  var userBlock = document.querySelector('.user-block');
+  var loginBtn = document.querySelector('.user-block__btn--login');
+  var cartBtn = document.querySelector('.user-block__btn--cart');
+  var toggleBtn = document.querySelector('.header__toggle');
+
+  var replaceSearch = function (currentVersion) {
+    var documentFragment = document.createDocumentFragment();
+    documentFragment.appendChild(search);
+    if (currentVersion !== 'desktop') {
+      menu.insertBefore(documentFragment, mainNav);
+    } else {
+      topContainer.appendChild(documentFragment);
+    }
+  };
+
+  var replaceLoginBtn = function (currentVersion) {
+    var documentFragment = document.createDocumentFragment();
+    documentFragment.appendChild(loginBtn);
+    if (currentVersion !== 'desktop') {
+      menu.appendChild(documentFragment);
+    } else {
+      userBlock.insertBefore(documentFragment, cartBtn);
+    }
+  };
+
+  var toggleMenu = function () {
+    pageHeader.classList.toggle('header--menu-opened');
+    userBlock.classList.toggle('user-block--menu-opened');
+  };
+
+  toggleBtn.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    toggleMenu();
+  });
+
+  window.pageHeader = {
+    elem: pageHeader,
+    userBlock: userBlock,
+    replaceSearch: replaceSearch,
+    replaceLoginBtn: replaceLoginBtn,
+  };
+})();
+
+(function () {
   var isStorageSupport = true;
 
   var loginForm = document.querySelector('.login-popup__form');
@@ -293,55 +342,6 @@
   window.loginPopup = {
     elem: loginPopup,
     init: initLoginPopup
-  };
-})();
-
-(function () {
-  var pageHeader = document.querySelector('.header');
-  var topContainer = document.querySelector('.header__top-container');
-  var menu = document.querySelector('.header__menu');
-  var mainNav = document.querySelector('.header__nav');
-  var search = document.querySelector('.search');
-  var userBlock = document.querySelector('.user-block');
-  var loginBtn = document.querySelector('.user-block__btn--login');
-  var cartBtn = document.querySelector('.user-block__btn--cart');
-  var toggleBtn = document.querySelector('.header__toggle');
-
-  var replaceSearch = function (currentVersion) {
-    var documentFragment = document.createDocumentFragment();
-    documentFragment.appendChild(search);
-    if (currentVersion !== 'desktop') {
-      menu.insertBefore(documentFragment, mainNav);
-    } else {
-      topContainer.appendChild(documentFragment);
-    }
-  };
-
-  var replaceLoginBtn = function (currentVersion) {
-    var documentFragment = document.createDocumentFragment();
-    documentFragment.appendChild(loginBtn);
-    if (currentVersion !== 'desktop') {
-      menu.appendChild(documentFragment);
-    } else {
-      userBlock.insertBefore(documentFragment, cartBtn);
-    }
-  };
-
-  var toggleMenu = function () {
-    pageHeader.classList.toggle('header--menu-opened');
-    userBlock.classList.toggle('user-block--menu-opened');
-  };
-
-  toggleBtn.addEventListener('click', function (evt) {
-    evt.preventDefault();
-    toggleMenu();
-  });
-
-  window.pageHeader = {
-    elem: pageHeader,
-    userBlock: userBlock,
-    replaceSearch: replaceSearch,
-    replaceLoginBtn: replaceLoginBtn,
   };
 })();
 
